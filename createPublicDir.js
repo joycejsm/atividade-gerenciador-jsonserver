96eaca3fddc3e3ac9b247d0601c1670187728b5a
Joyce Silva <joycejsm96@protonmail.com>
2024-10-15 19:10:56 -0300

    .

--- createPublicDir.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = './public';
-
-if (!fs.existsSync(path)){
-    fs.mkdirSync(path);
-    console.log('Public directory created.');
-} else {
-    console.log('Public directory already exists.');
-}
